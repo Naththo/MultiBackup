49097c7b435e62b9bab44cc8e3b0a7755e83b42c
Nathan <Naththo@hotmail.com>
2015-06-21 23:35:21 +0100

    Add destination

--- backup.sh
+++ backup.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-#required (mysql, file, directory)
+# == required
+
+# dest - destination of the backups
+dest=
+# type (mysql, file, directory)
 type=
 
-#file variables
+
+
+# == file variables
 file=
 
 function usage {
 	printf """Usage:
-	%s -t TYPE
+	%s -t TYPE -dest DEST
 
 	TYPE:
 		mysql		For MySQL databases
 		file		For a single file
 		directory	For a directory
+
+	DEST:
+		Directory where the backups will be
 """ "$0"
+	exit 1
 }
 
 
@@ -22,6 +32,9 @@ while getopts t:f: opt; do
 	case $opt in
 	t)
 		type=$OPTARG
+		;;
+	dest)
+		dest=$OPTARG
 		;;
 	f)
 		file=$OPTARG
@@ -31,7 +44,7 @@ done
 
 shift $((OPTIND -1))
 
-if [ -z "$type" ]; then
+if [ -z "$type" ] || [ -z "$dest" ]; then
 	usage
 fi
 
@@ -39,4 +52,12 @@ if [ "$type" == "file" ]; then
 	if [ -z "$file" ]; then
 		usage
 	fi
+
+	# check if file exists
+	if [ ! -f "$file" ]; then
+		echo "ERROR: $file does NOT exist"
+		exit 1
+	fi
+
+
 fi
